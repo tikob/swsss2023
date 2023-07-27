789b0fd8ababacefa1705f0007ac0d6660a98f96
Tinatin Baratashvili <tinatin.baratashvili@kuleuven.be>
2023-07-27 15:04:06 +0200

    calculation of the number of storms

--- day_02/plot_omni_data.py
+++ day_02/plot_omni_data.py
@@ -13,28 +13,145 @@ minute = []
 dst = []
 datetime_list = []
 
+def skip_condition(line):
+    """
+        This function skips the header in the file if it exists.
 
-"""
-This block opens the file, reads the lines and splits them.
-It creates datetie objects from the given data.
-And saves datetimes as a list.
-"""
-with open("omni_min_def_wdjmVKe8ea.lst") as f:
-    for line in f:
-        tmp = line.split()
-        dst.append(int(tmp[4]))
-        datetime1 = dt.datetime(int(tmp[0]), 1, 1, int(tmp[2]), int(tmp[3]))+dt.timedelta(days=int(tmp[1])-1)
-        datetime_list.append(datetime1)
-    # print (dathetime_list)
+        If the first character of the line is not a difit it returns False
+        If the first character of the line is it returns  True
 
-"""
-Plotting of the data with the label and rotated X-Axis for times.
-"""
-fig,ax = plt.subplots()
-ax.plot(datetime_list, dst, label="OMNI high res")
-ax.set_ylabel("SYMH (nT)")
-ax.set_xlabel("Time")
-ax.set_title("Geomagnetic Storm in 17 March 2013")
-ax.legend()
-plt.xticks(rotation=40)
-plt.show()
+        ATTENTION: this does not check every character,
+        so if the first one is digit and the others strings, it will not work.
+        This is because negative numbers are not digits.
+
+    """
+
+    if (not(line[0].isdigit())):
+        return False
+    return True
+
+
+
+def read_ascii_file(filename,index):
+    """
+    This function returns cthe given index from the data file - filename.
+    Inputs:
+        filename (string): OMNI data file
+        index (int): index of the variable to plot in the file
+    Returns:
+        Dictionary of datetime objects and the requested index.
+
+    """
+    dictionary_datetime_index = {}
+    datetimes = []
+    symh = []
+    with open(filename) as f:
+        for line in f:
+            if (skip_condition(line)):
+                tmp = line.split()
+                datetime1 = dt.datetime(int(tmp[0]), 1, 1, int(tmp[2]), int(tmp[3]))+dt.timedelta(days=int(tmp[1])-1)
+                datetimes.append(datetime1)
+                symh.append(int(tmp[index]))
+        dictionary_datetime_index = {'times': datetimes,'symh': symh}
+
+    return dictionary_datetime_index
+
+def trim_database(dictionary, start_time, end_time):
+    """
+        This functions trims the dictionary between the start and end dates.
+        Inputs:
+            dictionary (dictionary type): has keys {'times', 'symh'}
+            start_time (datetime object): the start time for trimming the dataset
+            end_time (datetime object): the end time for trimming the dataset
+        Retuns:
+            Dictionary that has trimmed dataset in it between start_date and end_date.
+    """
+    new_dictionary = {}
+    time = np.array(dictionary['times'])
+    symh = np.array(dictionary['symh'])
+
+    booleans_shorttime = (time>start_time)&(time<end_time)
+
+    time_trimmed=time[booleans_shorttime]
+    symh_trimmed=symh[booleans_shorttime]
+
+    new_dictionary['times'] = time_trimmed
+    new_dictionary['symh'] = symh_trimmed
+    return new_dictionary
+
+
+
+def count_geomagnetic_storms(dictionary,dst_reference,storm_duration):
+    symh = np.array(dictionary['symh'])
+    time = np.array(dictionary['times'])
+
+    counter = 0
+    storm_found = False
+    # Need to save start and end times and count as a storm only if it lasts 12hours
+    last_storm_start_time = time[0]
+    for i in range(len(symh)):
+        if (symh[i] < dst_reference and not(storm_found)):
+            storm_start_time = time[i]
+
+            current_storm_duration = (storm_start_time-last_storm_start_time).total_seconds()/3600.
+            if (current_storm_duration < storm_duration):
+                # print ( current_storm_duration )
+                continue
+            counter = counter+1
+
+            last_storm_start_time = storm_start_time
+            storm_found = True
+
+        elif(storm_found and symh[i] > dst_reference):
+            storm_found=False
+
+
+
+    # storm_occurance = (symh<dst_reference)
+    #
+    # storm_times =time[storm_occurance]
+    # storm_symh = symh[storm_occurance]
+    # dictionary['times'] = storm_times
+    # dictionary['symh'] = storm_symh
+    # counter = 0
+    # storm_found = False
+    # for symh_value in dictionary['symh']:
+    #     if (symh_value < dst_reference and not(storm_found)):
+    #         # print (symh_value)
+    #         counter = counter+1
+    #         storm_found = True
+    #     elif(storm_found and symh_value > dst_reference):
+    #         # print ("when turning false", symh_value)
+    #         storm_found = False
+
+    return counter, dictionary
+# This is the main code
+
+if __name__ == "__main__":
+    filename = "omni_min_def_wdjmVKe8ea.lst"
+    filename_2003 = "2003_symh_data.lst"
+    # start_plot_time = dt.datetime(2013, 3, 17)
+    # end_plot_time = dt.datetime(2013, 3, 18)
+    start_plot_time = dt.datetime(2003, 10, 28)
+    end_plot_time = dt.datetime(2003, 10, 29)
+    datetime_dst_dict = read_ascii_file(filename_2003, -1)
+    dst_reference = -100
+    storm_duration = 12
+    number_of_storms, storms_in_2003 = count_geomagnetic_storms(datetime_dst_dict,dst_reference,storm_duration)
+    print (number_of_storms)
+    # new_dictionary = datetime_dst_dict
+    # datetime_dict_toplot = trim_database(datetime_dst_dict, start_plot_time, end_plot_time)
+
+
+    # time
+    # short_period =
+    fig,ax = plt.subplots()
+    ax.plot(datetime_dst_dict['times'], datetime_dst_dict['symh'], label="OMNI high res")
+    ax.set_ylabel("SYMH (nT)")
+    ax.set_xlabel("Time")
+    ax.set_title("Geomagnetic Storm in 17 March 2013")
+    # ax.set_ylim(-100,-150)
+    # ax.set_xlim(start_plot_time,end_plot_time)
+    ax.legend()
+    plt.xticks(rotation=40)
+    plt.show()
--- day_03/Day_3_afternoon.py
+++ day_03/Day_3_afternoon.py
@@ -3,13 +3,13 @@
 """
 Welcome to Space Weather Simulation Summer School Day 3
 
-Today, we will be working with various file types, doing some simple data 
+Today, we will be working with various file types, doing some simple data
 manipulation and data visualization
 
-We will be using a lot of things that have been covered over the last two days 
+We will be using a lot of things that have been covered over the last two days
 with minor vairation.
 
-Goal: Getting comfortable with reading and writing data, doing simple data 
+Goal: Getting comfortable with reading and writing data, doing simple data
 manipulation, and visualizing data.
 
 Task: Fill in the cells with the correct codes
@@ -17,7 +17,7 @@ Task: Fill in the cells with the correct codes
 @author: Peng Mun Siew
 """
 
-#%% 
+#%%
 """
 This is a code cell that we can use to partition our data. (Similar to Matlab cell)
 We hace the options to run the codes cell by cell by using the "Run current cell" button on the top.
@@ -33,19 +33,23 @@ import numpy as np
 
 # Generate a random array of dimension 10 by 5
 data_arr = np.random.randn(10,5)
-print(data_arr)
+# print(data_arr)
 
 #%%
 """
 TODO: Writing and reading numpy file
 """
 # Save the data_arr variable into a .npy file
+np.save('test_np_save.npy', data_arr)
 
 
 # Load data from a .npy file
+data_arr_loaded = np.load('test_np_save.npy')
 
 
 # Verify that the loaded data matches the initial data
+# print (np.equal(data_arr,data_arr_loaded))
+# print(data_arr==data_arr_loaded)
 
 #%%
 """
@@ -53,13 +57,20 @@ TODO: Writing and reading numpy zip archive/file
 """
 # Generate a second random array of dimension 8 by 1
 data_arr2 = np.random.randn(8,1)
-print(data_arr2)
+# print(data_arr2)
 
 # Save the data_arr and data_arr2 variables into a .npz file
+np.savez('test_savez.npz', data_arr,data_arr2)
 
 # Load the numpy zip file
+npzfile = np.load('test_savez.npz')
+# print(npzfile)
+# print ("Variable names within this file: ", sorted(npzfile.files))
 
 # Verify that the loaded data matches the initial data
+# print(npzfile['arr_0'])
+# print((data_arr==npzfile['arr_0']).all())
+# print((data_arr2==npzfile['arr_1']).all())
 
 #%%
 """
@@ -67,34 +78,44 @@ Error and exception
 """
 # Exception handling, can be use with assertion as well
 try:
-    # Python will try to execute any code here, and if there is an exception 
-    # skip to below 
+    # Python will try to execute any code here, and if there is an exception
+    # skip to below
     print(np.equal(data_arr,npzfile).all())
 except:
     # Execute this code when there is an exception (unable to run code in try)
     print("The codes in try returned an error.")
     print(np.equal(data_arr,npzfile['arr_0']).all())
-    
-#%%
+
+# #%%
 """
 TODO: Error solving 1
 """
-# What is wrong with the following line? 
-np.equal(data_arr,data_arr2)
+# What is wrong with the following line?
+try:
+    np.equal(data_arr,data_arr2)
+except:
+    print ("This arrays are different.")
 
 #%%
 """
 TODO: Error solving 2
 """
-# What is wrong with the following line? 
-np.equal(data_arr2,npzfile['data_arr2'])
+# What is wrong with the following line?
+try:
+    np.equal(data_arr2,npzfile['data_arr2'])
+except:
+    print("This is not the key in npz file")
 
-#%%
+# #%%
 """
 TODO: Error solving 3
 """
-# What is wrong with the following line? 
-numpy.equal(data_arr2,npzfile['arr_1'])
+# What is wrong with the following line?
+try:
+    numpy.equal(data_arr2,npzfile['arr_1'])
+except:
+    print("We have imported numpy as np.")
+    np.equal(data_arr2,npzfile['arr_1'])
 
 
 #%%
@@ -106,7 +127,7 @@ Loading data from Matlab
 import numpy as np
 from scipy.io import loadmat
 
-dir_density_Jb2008 = 'Data/JB2008/2002_JB2008_density.mat'
+dir_density_Jb2008 = 'JB2008/2002_JB2008_density.mat'
 
 # Load Density Data
 try:
@@ -130,25 +151,26 @@ Let's visualize the density field for 400 KM at different time.
 # Import required packages
 import matplotlib.pyplot as plt
 
-# Before we can visualize our density data, we first need to generate the 
-# discretization grid of the density data in 3D space. We will be using 
+# Before we can visualize our density data, we first need to generate the
+# discretization grid of the density data in 3D space. We will be using
 # np.linspace to create evenly sapce data between the limits.
 
 localSolarTimes_JB2008 = np.linspace(0,24,24)
 latitudes_JB2008 = np.linspace(-87.5,87.5,20)
 altitudes_JB2008 = np.linspace(100,800,36)
-nofAlt_JB2008 = altitudes_JB2008.shape[0]
+nofAlt_JB2008 = altitudes_JB2008.shape[0] #nof stands for number of
 nofLst_JB2008 = localSolarTimes_JB2008.shape[0]
 nofLat_JB2008 = latitudes_JB2008.shape[0]
 
-# We can also impose additional constratints such as forcing the values to be integers.
-time_array_JB2008 = np.linspace(0,8760,5, dtype = int)
 
-# For the dataset that we will be working with today, you will need to reshape 
+# We can also impose additional constratints such as forcing the values to be integers.
+time_array_JB2008 = np.linspace(0,8759,5, dtype = int)
+# print(time_array_JB2008)
+# For the dataset that we will be working with today, you will need to reshape
 # them to be lst x lat x altitude
 JB2008_dens_reshaped = np.reshape(JB2008_dens,(nofLst_JB2008,nofLat_JB2008,
                                                nofAlt_JB2008,8760), order='F') # Fortran-like index order
-
+# print(JB2008_dens_reshaped.shape)
 #%%
 """
 TODO: Plot the atmospheric density for 400 KM for the first time index in
@@ -157,9 +179,24 @@ TODO: Plot the atmospheric density for 400 KM for the first time index in
 
 import matplotlib.pyplot as plt
 
+
+
 # Look for data that correspond to an altitude of 400 KM
 alt = 400
 hi = np.where(altitudes_JB2008==alt)
+
+fig, axs = plt.subplots(5, figsize=(15,30), sharex=True)
+# help(plt.contourf)
+# print (JB2008_dens_reshaped[:,:,hi,time_array_JB2008[0]])
+print(len(time_array_JB2008))
+for i in range(len(time_array_JB2008)):
+    print (i)
+    axs[i].contourf(localSolarTimes_JB2008,latitudes_JB2008,JB2008_dens_reshaped[:,:,hi,time_array_JB2008[i]].squeeze().T)
+    cs = plt.contourf(localSolarTimes_JB2008,latitudes_JB2008,JB2008_dens_reshaped[:,:,hi,time_array_JB2008[i]].squeeze().T)
+    cbar = fig.colorbar(cs,ax=axs[i])
+    axs[i].set_title("JB2008 density at 400 km, t = "+str(time_array_JB2008[i])+" hrs")
+    cbar.ax.set_ylabel('Density')
+# plt.show()
 
 
 #%%
@@ -175,27 +212,53 @@ Assignment 1
 Can you plot the mean density for each altitude at February 1st, 2002?
 """
 
-# First identidy the time index that corresponds to  February 1st, 2002. 
+# First identidy the time index that corresponds to  February 1st, 2002.
 # Note the data is generated at an hourly interval from 00:00 January 1st, 2002
 time_index = 31*24
 dens_data_feb1 = JB2008_dens_reshaped[:,:,:,time_index]
-print('The dimension of the data are as followed 
-      (local solar time,latitude,altitude):', JB2008_dens_feb1.shape)
 
+print('The dimension of the data are as followed (local solar time,latitude,altitude):', dens_data_feb1.shape)
+
+
+# mean_densities = [dens_data_feb1[:,:,i].mean() for i in range(nofAlt_JB2008)]
+mean_densities_JB2008 = np.mean(dens_data_feb1,axis=(0,1))
+fig, ax = plt.subplots()
+ax.plot(altitudes_JB2008, mean_densities_JB2008)
+ax.set_xlabel("Altitude")
+ax.set_ylabel("Density")
+ax.set_title("Mean densities VS Altitude")
+ax.semilogy()
+plt.grid()
+# plt.show()
 #%%
 """
 Data Visualization II
 
-Now, let's us work with density data from TIE-GCM instead, and plot the density 
+Now, let's us work with density data from TIE-GCM instead, and plot the density
 field at 310km
 
 """
 # Import required packages
 import h5py
-loaded_data = h5py.File('Data/TIEGCM/2002_TIEGCM_density.mat')
-
+loaded_data = h5py.File('TIEGCM/2002_TIEGCM_density.mat')
 # This is a HDF5 dataset object, some similarity with a dictionary
 print('Key within dataset:',list(loaded_data.keys()))
+tiegcm_dens = (10**np.array(loaded_data['density'])*1000).T # convert from g/cm3 to kg/m3
+altitudes_tiegcm = np.array(loaded_data['altitudes']).flatten()
+latitudes_tiegcm = np.array(loaded_data['latitudes']).flatten()
+localSolarTimes_tiegcm = np.array(loaded_data['localSolarTimes']).flatten()
+nofAlt_tiegcm = altitudes_tiegcm.shape[0]
+nofLst_tiegcm = localSolarTimes_tiegcm.shape[0]
+nofLat_tiegcm = latitudes_tiegcm.shape[0]
+
+# We will be using the same time index as before.
+time_array_tiegcm = time_array_JB2008
+
+# Each data correspond to the density at a point in 3D space.
+# We can recover the density field by reshaping the array.
+# For the dataset that we will be working with today, you will need to reshape them to be lst x lat x altitude
+tiegcm_dens_reshaped = np.reshape(tiegcm_dens,(nofLst_tiegcm,nofLat_tiegcm,nofAlt_tiegcm,8760), order='F')
+
 
 
 #%%
@@ -203,108 +266,118 @@ print('Key within dataset:',list(loaded_data.keys()))
 TODO: Plot the atmospheric density for 310 KM for all time indexes in
       time_array_tiegcm
 """
-
-#%%
-"""
-Assignment 1.5
-
-Can you plot the mean density for each altitude at February 1st, 2002 for both 
-models (JB2008 and TIE-GCM) on the same plot?
-"""
-
-# First identidy the time index that corresponds to  February 1st, 2002. 
-# Note the data is generated at an hourly interval from 00:00 January 1st, 2002
-time_index = 31*24
-
-
-#%%
-"""
-Data Interpolation (1D)
-
-Now, let's us look at how to do data interpolation with scipy
-"""
-# Import required packages
-from scipy import interpolate
-
-# Let's first create some data for interpolation
-x = np.arange(0, 10)
-y = np.exp(-x/3.0)
+alt = 310
+hi_tiegcm = np.where(altitudes_tiegcm==alt)
+fig, axes = plt.subplots(5, figsize=(15,30), sharex=True)
+# help(plt.contourf)
+# print (JB2008_dens_reshaped[:,:,hi,time_array_JB2008[0]])
+print(len(time_array_JB2008))
+for i in range(len(time_array_JB2008)):
+    print (i)
+    axes[i].contourf(localSolarTimes_tiegcm,latitudes_tiegcm,tiegcm_dens_reshaped[:,:,hi_tiegcm,time_array_JB2008[i]].squeeze().T)
+    cs = plt.contourf(localSolarTimes_tiegcm,latitudes_tiegcm,tiegcm_dens_reshaped[:,:,hi_tiegcm,time_array_JB2008[i]].squeeze().T)
+    cbar = fig.colorbar(cs,ax=axes[i])
+    axes[i].set_title("TIE-GCM density at 310 km, t = "+str(time_array_JB2008[i])+" hrs")
+    cbar.ax.set_ylabel('Density')
+plt.show()
 
 
-#%%
-"""
-Data Interpolation (3D)
-
-Now, let's us look at how to do data interpolation with scipy
-"""
-# Import required packages
-from scipy.interpolate import RegularGridInterpolator
-
-# First create a set of sample data that we will be using 3D interpolation on
-def function_1(x, y, z):
-    return 2 * x**3 + 3 * y**2 - z
-
-x = np.linspace(1, 4, 11)
-y = np.linspace(4, 7, 22)
-z = np.linspace(7, 9, 33)
-xg, yg ,zg = np.meshgrid(x, y, z, indexing='ij', sparse=True)
-
-sample_data = function_1(xg, yg, zg)
-
-
-#%%
-"""
-Saving mat file
-
-Now, let's us look at how to we can save our data into a mat file
-"""
-# Import required packages
-from scipy.io import savemat
-
-a = np.arange(20)
-mdic = {"a": a, "label": "experiment"} # Using dictionary to store multiple variables
-savemat("matlab_matrix.mat", mdic)
-
-#%%
-"""
-Assignment 2 (a)
-
-The two data that we have been working on today have different discretization 
-grid.
-
-Use 3D interpolation to evaluate the TIE-GCM density field at 400 KM on 
-February 1st, 2002, with the discretized grid used for the JB2008 
-((nofLst_JB2008,nofLat_JB2008,nofAlt_JB2008).
-"""
-
-
-
-
-
-#%%
-"""
-Assignment 2 (b)
-
-Now, let's find the difference between both density models and plot out this 
-difference in a contour plot.
-"""
-
-
-
-
-
-#%%
-"""
-Assignment 2 (c)
-
-In the scientific field, it is sometime more useful to plot the differences in 
-terms of absolute percentage difference/error (APE). Let's plot the APE 
-for this scenario.
-
-APE = abs(tiegcm_dens-JB2008_dens)/tiegcm_dens
-"""
-
-
-
-
-
+# #%%
+# """
+# Assignment 1.5
+#
+# Can you plot the mean density for each altitude at February 1st, 2002 for both
+# models (JB2008 and TIE-GCM) on the same plot?
+# """
+#
+# # First identidy the time index that corresponds to  February 1st, 2002.
+# # Note the data is generated at an hourly interval from 00:00 January 1st, 2002
+# time_index = 31*24
+#
+#
+# #%%
+# """
+# Data Interpolation (1D)
+#
+# Now, let's us look at how to do data interpolation with scipy
+# """
+# # Import required packages
+# from scipy import interpolate
+#
+# # Let's first create some data for interpolation
+# x = np.arange(0, 10)
+# y = np.exp(-x/3.0)
+#
+#
+# #%%
+# """
+# Data Interpolation (3D)
+#
+# Now, let's us look at how to do data interpolation with scipy
+# """
+# # Import required packages
+# from scipy.interpolate import RegularGridInterpolator
+#
+# # First create a set of sample data that we will be using 3D interpolation on
+# def function_1(x, y, z):
+#     return 2 * x**3 + 3 * y**2 - z
+#
+# x = np.linspace(1, 4, 11)
+# y = np.linspace(4, 7, 22)
+# z = np.linspace(7, 9, 33)
+# xg, yg ,zg = np.meshgrid(x, y, z, indexing='ij', sparse=True)
+#
+# sample_data = function_1(xg, yg, zg)
+#
+#
+# #%%
+# """
+# Saving mat file
+#
+# Now, let's us look at how to we can save our data into a mat file
+# """
+# # Import required packages
+# from scipy.io import savemat
+#
+# a = np.arange(20)
+# mdic = {"a": a, "label": "experiment"} # Using dictionary to store multiple variables
+# savemat("matlab_matrix.mat", mdic)
+#
+# #%%
+# """
+# Assignment 2 (a)
+#
+# The two data that we have been working on today have different discretization
+# grid.
+#
+# Use 3D interpolation to evaluate the TIE-GCM density field at 400 KM on
+# February 1st, 2002, with the discretized grid used for the JB2008
+# ((nofLst_JB2008,nofLat_JB2008,nofAlt_JB2008).
+# """
+#
+#
+#
+#
+#
+# #%%
+# """
+# Assignment 2 (b)
+#
+# Now, let's find the difference between both density models and plot out this
+# difference in a contour plot.
+# """
+#
+#
+#
+#
+#
+# #%%
+# """
+# Assignment 2 (c)
+#
+# In the scientific field, it is sometime more useful to plot the differences in
+# terms of absolute percentage difference/error (APE). Let's plot the APE
+# for this scenario.
+#
+# APE = abs(tiegcm_dens-JB2008_dens)/tiegcm_dens
+# """
