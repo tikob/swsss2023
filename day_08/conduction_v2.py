d6f30daf483fdb58005250444e39bac335ed084c
Tinatin Baratashvili <tinatin.baratashvili@kuleuven.be>
2023-08-02 20:37:26 +0200

    the code with f10.7 and tides

--- day_08/conduction_v2.py
+++ day_08/conduction_v2.py
@@ -9,30 +9,49 @@ from tridiagonal import solve_tridiagonal
 # ----------------------------------------------------------------------
 
 if __name__ == "__main__":
+    fig = plt.figure(figsize = (10,10))
+    ax = fig.add_subplot(111)
 
     dx = 0.25
 
     # set x with 1 ghost cell on both sides:
     x = np.arange(-dx, 10. + 2 * dx, dx)
     alt = 100+40*x
+    dalt = alt[1]-alt[0]
+    # fr = dalt/dx
+    # print ("fraction", fr,  100./fr)
+
+    x = alt
+    dx = dalt
 
 
     t_lower = 200.0
     t_upper = 1500.0
 
     nPts = len(x)
-    nDays = 12
-    dt = 0.5
+    nDays = 27
+    dt = 1
     times = np.arange(0, nDays*24.0, dt)
+    f10_7 = 100 + 50*times/(365.*24.)+ 25.*np.sin(times/(27*24)*2*np.pi)
+    # print ()
+    # ax.plot(times, f10_7)
+
+    # ax.set_xlabel("times")
+    # ax.set_ylabel("F 10.7")
+    AmpDi = 10.
+    AmpSd = 5.
+    PhaseDi = np.pi/2
+    PhaseSd = 3*np.pi/2
 
     lon = 44.8015
-    fig = plt.figure(figsize = (10,10))
-    ax = fig.add_subplot(111)
+
     temperatures = np.zeros([len(times), nPts])
-    print (len(temperatures))
     for counter,hour in enumerate(times):
+
         ut = hour%24
         LT = lon/15+ut
+        t_lower = 200.0 + AmpDi*np.sin(LT/24*2*np.pi+PhaseDi) + AmpSd*np.sin(LT/24*np.pi*2*2 + PhaseSd)
+        # f10_7 = 100+50*hour/(365.*24.)+ 25.np.sin(hour/27*2*np.pi)
         # print (ut)
         factor = -np.cos(LT/24*np.pi*2)
         # print (factor)
@@ -43,15 +62,15 @@ if __name__ == "__main__":
         d = np.zeros(nPts)
 
         # Add a source term:
-        lam = 10.
+        lam = 80. #10.
         dz = x[1]-x[0]
         dz2 = dz*dz
 
         qback = np.zeros(nPts)
-        qback[(x>3)&(x<7)] = 100.
-        Sunheat = 100.
+        qback[(x>200)&(x<400)] = 0.4
+        Sunheat =0.4*f10_7[counter]/100  #100./fr**2
         qeuv = np.zeros(nPts)
-        qeuv[(x>3)&(x<7)] = (factor*Sunheat)
+        qeuv[(x>200)&(x<400)] = (factor*Sunheat)
         d = (qback+qeuv)*dz2/lam
 
         # boundary conditions (bottom - fixed):
@@ -79,7 +98,7 @@ if __name__ == "__main__":
         # ax.set_xlabel("x")
         # ax.set_ylabel("Temperature")
 
-    CS = ax.contourf(times/24.,alt, temperatures.T, cmap = 'plasma')
+    CS = ax.contourf(times/24.,x, temperatures.T, cmap = 'viridis')
     cbat = fig.colorbar(CS)
     cbat.ax.set_ylabel('T')
     ax.set_ylabel("Alt")
